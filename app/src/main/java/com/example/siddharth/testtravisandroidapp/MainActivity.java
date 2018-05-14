bbb7d8b6de2b2da47bfe23a6fb63769c6285d47e
siddharth2010 <ssiddharth2010@gmail.com>
2018-05-14 13:28:08 +0530

    testing package wide movements with CI

--- app/src/main/java/com/example/siddharth/testtravisandroidapp/MainActivity.java
+++ app/src/main/java/com/example/siddharth/testtravisandroidapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.siddharth.testtravisandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.siddharth.testtravisandroidapp.Utils.TextUtils.TextUtilsClass;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    TextView mainText = (TextView) findViewById(R.id.main_text);
+    String textToFitIn = TextUtilsClass.gettingText();
+    mainText.setText(textToFitIn);
   }
 }
---
+++ app/src/main/java/com/example/siddharth/testtravisandroidapp/Utils/TextUtils/TextUtilsClass.java
@@ -0,0 +1,13 @@
+package com.example.siddharth.testtravisandroidapp.Utils.TextUtils;
+
+/**
+ * Created by siddharth on 5/14/2018.
+ */
+
+public class TextUtilsClass {
+
+  public static String gettingText(){
+    return "This is some random text!";
+  }
+
+}
